2e685f76ffb513752b15e8474e9bd94e3fb362e1
example <tranhobao0914705078@gmail.com>
2023-04-12 01:26:13 +0700

    add screens

---
+++ frontend/src/screens/CategoryScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function CategoryScreen() {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
+
+export default CategoryScreen
