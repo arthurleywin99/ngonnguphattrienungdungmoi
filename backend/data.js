0354aa94b7c2b5f4fdd682a862c4f2258618496d
Bui Nguyen Truong Giang <buinguyentruonggiang.1112@gmail.com>
2023-04-12 00:24:49 +0700

    Add dât and utils

---
+++ backend/data.js
@@ -0,0 +1,212 @@
+export const brandSeed = [
+  {
+    name: 'Samsung',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340350/Samsung_omqjcn.png',
+  },
+  {
+    name: 'Vivo',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340350/Vivo_vvsfem.png',
+  },
+  {
+    name: 'Oppo',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340350/Oppo_jb9owo.jpg',
+  },
+  {
+    name: 'Tcl',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340350/Tcl_yinyrw.jpg',
+  },
+  {
+    name: 'Realme',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340350/Realme_cb84j3.png',
+  },
+  {
+    name: 'Masstel',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340349/Masstel_hjpjwc.png',
+  },
+  {
+    name: 'Xiaomi',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340349/Xiaomi_d6h7qu.png',
+  },
+  {
+    name: 'Mobell',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340349/Mobell_dtmazz.jpg',
+  },
+  {
+    name: 'iPhone',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340349/iPhone_dqipr3.png',
+  },
+  {
+    name: 'Itel',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340349/Itel_oitzei.jpg',
+  },
+  {
+    name: 'Nokia',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676340349/Nokia_z1ao67.jpg',
+  },
+]
+
+export const productsSeed = [
+  {
+    name: 'OPPO Reno8 T 5G 256GB',
+    category: 'Laptops',
+    brand: '63eaf713494a98df7eeb1cc7',
+    images: [
+      '/images/product-list1.jpg',
+      '/images/product-list1.jpg',
+      '/images/product-list1.jpg',
+    ],
+    price: 990000,
+    discount: -14,
+    comments: [],
+    countInStock: 1000,
+    description: 'Tặng loa Olike S2 1.59 Triệu + Trả góp 0%',
+    settings: [
+      {
+        key: 'Màn hình',
+        value: 'Dynamic AMOLED 2X6.8"Quad HD+ (2K+)',
+      },
+      {
+        key: 'Hệ điều hành',
+        value: 'Android 13',
+      },
+      {
+        key: 'Camera sau',
+        value: 'Chính 200 MP & Phụ 12 MP, 10 MP, 10 MP',
+      },
+      {
+        key: 'Camera trước',
+        value: '12MP',
+      },
+      {
+        key: 'Chip',
+        value: 'Snapdragon 8 Gen 2 8 nhân',
+      },
+      {
+        key: 'RAM',
+        value: '8GB',
+      },
+      {
+        key: 'Dung lượng lưu trữ',
+        value: '256GB',
+      },
+      {
+        key: 'SIM',
+        value: '2 Nano SIM hoặc 1 Nano SIM + 1 eSIMHỗ trợ 5G',
+      },
+      {
+        key: 'Pin, Sạc',
+        value: '5000 mAh, 45 W',
+      },
+    ],
+    status: true,
+  },
+]
+
+export const webInfosSeed = [
+  {
+    name: 'Medium Banner',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676388590/banner_hgabwr.png',
+  },
+  {
+    name: 'Deal Banner',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676388621/mid-banner-2_gzgenn.png',
+  },
+  {
+    name: 'Big Banner',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676388560/midbanner_iqbzty.jpg',
+  },
+  {
+    name: 'Monopoly',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676388604/Label_01-05_foooch.png',
+  },
+  {
+    name: 'Logo',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676388606/logo_qixiw4.jpg',
+  },
+  {
+    name: 'Rewards Icon 50x50',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676388598/icon3-50x50_levuop.png',
+  },
+  {
+    name: 'Slider 1',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676421340/Slider1_tsxegs.webp',
+  },
+  {
+    name: 'Slider 2',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676421340/Slider2_htadmo.webp',
+  },
+  {
+    name: 'Slider 3',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676421340/Slider3_liunji.webp',
+  },
+  {
+    name: 'Slider 4',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676421340/Slider4_tt5wpw.webp',
+  },
+  {
+    name: 'Slider 5',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676421340/Slider5_lufqy0.webp',
+  },
+  {
+    name: 'Slider 6',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1676421340/Slider6_seqthj.webp',
+  },
+  {
+    name: 'VNPay',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1677813756/VNPay_Logo_scv0p7.png',
+  },
+  {
+    name: 'MOMO',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1677813756/MoMo_Logo_ghoiwx.png',
+  },
+  {
+    name: 'Paypal',
+    image:
+      'https://res.cloudinary.com/gb-hutech/image/upload/v1677813756/PayPal_Logo_f0vpb0.png',
+  },
+]
+
+export const userSeed = [
+  {
+    name: 'Giang',
+    email: 'giang1@gmail.com',
+    phoneNumber: '',
+    password: 'Aa@123',
+    profilePicUrl: '',
+    address: 'HCM City',
+    isAdmin: false,
+    isSeller: false,
+    seller: {
+      name: '',
+      logo: '',
+      description: '',
+      averageRating: 0,
+      numReviews: 0,
+    },
+  },
+]
